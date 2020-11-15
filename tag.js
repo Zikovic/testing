cca907efebed31c183616524b7ff8291baa1032c
BEN ALLAL Zakariae <benallal.zakariae@gmail.com>
2020-11-15 13:05:41 +0100

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,2 +1,6 @@
 alert("hello script tags");
 console.log("https://raw.githack.com/Zikovic/testing/master/scriptshopify.js");
+
+
+console.log(Shopify.checkout.order_id);
+alert(Shopify.checkout.order_id)
